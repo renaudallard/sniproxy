012aee43f516131b263402fab4e6a0e3587afc9b
Renaud Allard <renaud@allard.it>
2025-11-01 10:51:14 +0100

    Unbreak Debian builds as per request.
    This reverts commit 12bd55694df625b45ffcae010d35423059a83561.

--- setver.sh
+++ setver.sh
@@ -19,6 +19,8 @@ if [ -d ${GIT_DIR} ]; then
         else
             # Release version (e.g. 0.3.5)
             VERSION=${GIT_VERSION}
+            DEBIAN_VERSION=${VERSION}
+            SPEC_VERSION=${VERSION}
         fi
     fi
 fi
@@ -26,3 +28,8 @@ fi
 # Update Autoconf with new version
 sed -i "s/^\(AC_INIT(\[sniproxy\], \[\)[^]]*\(.\+\)$/\1${VERSION}\2/" ${SOURCE_DIR}/configure.ac
 
+# Update redhat/sniproxy.spec with new version
+sed -i "s/^Version:\s\+[^ ]\+/Version: ${VERSION}/" ${SOURCE_DIR}/redhat/sniproxy.spec
+
+# Update debian/changelog with new version
+debchange --newversion ${VERSION} "New git revision"
