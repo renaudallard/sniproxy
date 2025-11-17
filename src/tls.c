renaudallard/sniproxy

commit 6ab47bb5892b8b9b40dd4a15be63fcf05774bf89

Fix some checks.

src/tls.c
@@ -52,10 +52,10 @@
 
 
 static int parse_tls_header(const uint8_t*, size_t, char **);
-static int parse_extensions(const uint8_t*, size_t, char **,
-        uint8_t required_major, uint8_t required_minor,
-        int require_supported_versions, int *version_ok);
+static int parse_extensions(const uint8_t*, size_t, char **);
 static int parse_server_name_extension(const uint8_t*, size_t, char **);
+static int extensions_have_required_version(const uint8_t *, size_t,
+        uint8_t required_major, uint8_t required_minor);
 static int parse_supported_versions_extension(const uint8_t *, size_t,
         uint8_t required_major, uint8_t required_minor,
         int require_supported_versions, int *version_ok);
@@ -197,8 +197,6 @@ parse_tls_header(const uint8_t *data, size_t data_len, char **hostname) {
 
     int require_supported_versions = (min_client_hello_version_major > 3) ||
         (min_client_hello_version_major == 3 && min_client_hello_version_minor >= 4);
-    int supported_version_ok = require_supported_versions ? 0 : 1;
-
     body += CLIENT_HELLO_VERSION_RANDOM_LEN;
 
     /* Session ID */
@@ -208,7 +206,6 @@ parse_tls_header(const uint8_t *data, size_t data_len, char **hostname) {
     body += 1;
     if ((size_t)(body_end - body) < len)
         return -5;
-    body += len;
 
     /* Cipher Suites */
     if ((size_t)(body_end - body) < 2)
@@ -241,64 +238,46 @@ parse_tls_header(const uint8_t *data, size_t data_len, char **hostname) {
 
     if ((size_t)(body_end - body) < len)
         return -5;
-    int ext_result = parse_extensions(body, len, hostname,
-            min_client_hello_version_major, min_client_hello_version_minor,
-            require_supported_versions, &supported_version_ok);
-    if (ext_result == TLS_ERR_UNSUPPORTED_CLIENT_HELLO)
-        return ext_result;
-    if (ext_result < -1)
-        return ext_result;
-    if (require_supported_versions && !supported_version_ok)
-        return TLS_ERR_UNSUPPORTED_CLIENT_HELLO;
-    return ext_result;
+
+    if (require_supported_versions) {
+        int sv = extensions_have_required_version(body, len,
+                min_client_hello_version_major, min_client_hello_version_minor);
+        if (sv == TLS_ERR_UNSUPPORTED_CLIENT_HELLO)
+            return sv;
+        if (sv < 0)
+            return sv;
+        if (sv == 0)
+            return TLS_ERR_UNSUPPORTED_CLIENT_HELLO;
+    }
+
+    return parse_extensions(body, len, hostname);
 }
 
 static int
-parse_extensions(const uint8_t *data, size_t data_len, char **hostname,
-        uint8_t required_major, uint8_t required_minor,
-        int require_supported_versions, int *version_ok) {
+parse_extensions(const uint8_t *data, size_t data_len, char **hostname) {
     size_t pos = 0;
     size_t len;
     size_t extension_count = 0;
-    int server_name_result = -2;
 
-    /* Parse each 4 bytes for the extension header */
     while (pos <= data_len) {
         size_t remaining = data_len - pos;
         if (remaining < 4)
             break;
 
-        /* Limit number of extensions to prevent CPU exhaustion attacks */
         if (extension_count >= TLS_MAX_EXTENSIONS) {
             debug("TLS ClientHello exceeded maximum extension count (%d)", TLS_MAX_EXTENSIONS);
             return -5;
         }
         extension_count++;
 
-        /* Extension Length */
         len = ((size_t)data[pos + 2] << 8) +
             (size_t)data[pos + 3];
 
         if (len > remaining - 4)
             return -5;
 
-        /* Check if it's a server name extension */
-        if (data[pos] == 0x00 && data[pos + 1] == 0x00) {
-            int sni_result = parse_server_name_extension(data + pos + 4, len, hostname);
-            if (sni_result < -1)
-                return sni_result;
-            server_name_result = sni_result;
-        }
-
-        if (data[pos] == 0x00 && data[pos + 1] == 0x2b) {
-            int sv = parse_supported_versions_extension(data + pos + 4, len,
-                    required_major, required_minor,
-                    require_supported_versions, version_ok);
-            if (sv == TLS_ERR_UNSUPPORTED_CLIENT_HELLO)
-                return sv;
-            if (sv < 0)
-                return -5;
-        }
+        if (data[pos] == 0x00 && data[pos + 1] == 0x00)
+            return parse_server_name_extension(data + pos + 4, len, hostname);
 
         if (data[pos] == 0xff && data[pos + 1] == 0x01) {
             if (len < 1)
@@ -315,16 +294,47 @@ parse_extensions(const uint8_t *data, size_t data_len, char **hostname,
             }
         }
 
-        pos += 4 + len; /* Advance to the next extension header */
+        pos += 4 + len;
     }
-    /* Check we ended where we expected to */
+
     if (pos != data_len)
         return -5;
 
-    if (require_supported_versions && version_ok != NULL && !*version_ok)
-        return TLS_ERR_UNSUPPORTED_CLIENT_HELLO;
+    return -2;
+}
 
-    return server_name_result;
+static int
+extensions_have_required_version(const uint8_t *data, size_t data_len,
+        uint8_t required_major, uint8_t required_minor) {
+    size_t pos = 0;
+    size_t len;
+
+    while (pos <= data_len) {
+        size_t remaining = data_len - pos;
+        if (remaining < 4)
+            break;
+
+        len = ((size_t)data[pos + 2] << 8) +
+            (size_t)data[pos + 3];
+
+        if (len > remaining - 4)
+            return -5;
+
+        if (data[pos] == 0x00 && data[pos + 1] == 0x2b) {
+            int version_ok = 0;
+            int rc = parse_supported_versions_extension(data + pos + 4, len,
+                    required_major, required_minor, 1, &version_ok);
+            if (rc == TLS_ERR_UNSUPPORTED_CLIENT_HELLO)
+                return rc;
+            if (rc < 0)
+                return rc;
+            return version_ok ? 1 : TLS_ERR_UNSUPPORTED_CLIENT_HELLO;
+        }
+
+        pos += 4 + len;
+    }
+
+    return 0;
 }
 
 static int

src/binder.c
@@ -159,11 +159,6 @@ binder_restart_child(void) {
         binder_cleanup_child(1);
     }
 
-    if (geteuid() != 0) {
-        err("cannot restart binder after privilege drop");
-        return -1;
-    }
-
     return binder_spawn_child();
 }
 
@@ -353,4 +348,3 @@ binder_main(int sockfd) {
         free(plain);
     }
 }
-

src/logger.c
@@ -808,7 +808,7 @@ obtain_file_sink(const char *filepath) {
             free((char *)sink->filepath);
             free(sink);
             errno = saved_errno;
-            err("Failed to open new log file: %s", filepath);
+            err("Failed to open new log file %s: %s", filepath, strerror(saved_errno));
             return NULL;
         }
 
@@ -912,7 +912,7 @@ obtain_file_sink(const char *filepath) {
             free((char *)sink->filepath);
             free(sink);
             errno = saved_errno;
-            err("Failed to open new log file: %s", filepath);
+            err("Failed to open new log file %s: %s", filepath, strerror(saved_errno));
             return NULL;
         }
 

tests/TestUtils.pm
@@ -119,7 +119,6 @@ sub make_config($$) {
     my $httpd_port = shift;
 
     my ($fh, $filename) = File::Temp::tempfile();
-    my ($unused, $logfile) = File::Temp::tempfile();
 
     # Write out a test config file
     print $fh <<END;
@@ -127,8 +126,6 @@ sub make_config($$) {
 
 listen 127.0.0.1 $proxy_port {
     proto http
-
-    access_log $logfile
 }
 
 table {

tests/address_test.c
@@ -136,7 +136,9 @@ int main(void) {
             return 1;
         }
 
-        assert(display_address(addr, NULL, 0) == NULL);
+        const char *invalid_display = display_address(addr, NULL, 0);
+        assert(invalid_display != NULL);
+        assert(strcmp(invalid_display, "(invalid)") == 0);
 
         port = address_port(addr);
 
@@ -197,4 +199,3 @@ int main(void) {
 
     return 0;
 }
-

tests/binder_test.c
@@ -58,7 +58,7 @@ test_binder(int port) {
 
     int fd = bind_socket((struct sockaddr *)&addr, sizeof(addr));
 
-    assert(fd > 0);
+    assert(fd >= 0);
 
     /* Verify we obtained the expected socket address */
     struct sockaddr_storage addr_verify;

tests/tls_test.c
@@ -91,7 +91,7 @@ const unsigned char good_data_2[] = {
     0x00, 0x48, // Length
         // Handshake
         0x01, // Handshake Type: Client Hello
-        0x00, 0x00, 0x42, // Length
+        0x00, 0x00, 0x44, // Length
         0x03, 0x03, // Version: TLS 1.2
         // Random
         0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
@@ -127,7 +127,7 @@ const unsigned char good_data_3[] = {
     0x00, 0x48, // Length
         // Handshake
         0x01, // Handshake Type: Client Hello
-        0x00, 0x00, 0x42, // Length
+        0x00, 0x00, 0x44, // Length
         0x03, 0x03, // Version: TLS 1.2
         // Random
         0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
@@ -162,7 +162,7 @@ const unsigned char good_data_4[] = {
     0x00, 0x47, // Length
         // Handshake
         0x01, // Handshake Type: Client Hello
-        0x00, 0x00, 0x41, // Length
+        0x00, 0x00, 0x43, // Length
         0x03, 0x03, // Version: TLS 1.2
         // Random
         0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
@@ -453,7 +453,7 @@ const unsigned char bad_data_4[] = {
     0x00, 0x48, // Length
         // Handshake
         0x01, // Handshake Type: Client Hello
-        0x00, 0x00, 0x42, // Length
+        0x00, 0x00, 0x44, // Length
         0x03, 0x03, // Version: TLS 1.2
         // Random
         0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
@@ -655,4 +655,3 @@ int main(void) {
 
     return 0;
 }
-

commit bbc5f36918bbf12688b0c9747b6a262b76c5ce0d

Fix tls bug introduced in 6ab47bb

src/tls.c
@@ -206,6 +206,7 @@ parse_tls_header(const uint8_t *data, size_t data_len, char **hostname) {
     body += 1;
     if ((size_t)(body_end - body) < len)
         return -5;
+    body += len;
 
     /* Cipher Suites */
     if ((size_t)(body_end - body) < 2)