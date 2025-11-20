4cfb92f00a492a639bb5806c26e46f697294c946
Renaud Allard <renaud@allard.it>
2025-11-20 18:24:57 +0100

    Add cleanup_keyword_context in cfg_parser.c to centralize releasing of temporary sub-contexts.

--- src/cfg_parser.c
+++ src/cfg_parser.c
@@ -34,6 +34,7 @@
 
 static int parse_config_depth(void *, FILE *, const struct Keyword *, const char *, int);
 static const struct Keyword *find_keyword(const struct Keyword *, const char *);
+static void cleanup_keyword_context(const struct Keyword *, void *, void *);
 
 
 int
@@ -61,25 +62,14 @@ parse_config_depth(void *context, FILE *cfg, const struct Keyword *grammar,
         switch (next_token(cfg, buffer, sizeof(buffer))) {
             case TOKEN_ERROR:
                 err("%s: tokenizer error", __func__);
-                /* Free sub_context if it was newly created */
-                if (keyword && keyword->create && sub_context && sub_context != context) {
-                    if (keyword->cleanup)
-                        keyword->cleanup(sub_context);
-                    else
-                        free(sub_context);
-                }
+                cleanup_keyword_context(keyword, context, sub_context);
                 return -1;
             case TOKEN_WORD:
                 if (keyword && sub_context && keyword->parse_arg) {
                     result = keyword->parse_arg(sub_context, buffer);
                     if (result <= 0) {
                         /* Free sub_context if it was newly created and parse_arg failed */
-                        if (keyword->create && sub_context != context) {
-                            if (keyword->cleanup)
-                                keyword->cleanup(sub_context);
-                            else
-                                free(sub_context);
-                        }
+                        cleanup_keyword_context(keyword, context, sub_context);
                         return result;
                     }
 
@@ -99,18 +89,14 @@ parse_config_depth(void *context, FILE *cfg, const struct Keyword *grammar,
                         result = keyword->parse_arg(sub_context, buffer);
                         if (result <= 0) {
                             /* Free sub_context if it was newly created and parse_arg failed */
-                            if (keyword->create && sub_context != context) {
-                                if (keyword->cleanup)
-                                    keyword->cleanup(sub_context);
-                                else
-                                    free(sub_context);
-                            }
+                            cleanup_keyword_context(keyword, context, sub_context);
                             return result;
                         }
                     }
                 } else {
                     err("%s: unknown keyword %s in %s context", __func__,
                             buffer, active_context);
+                    cleanup_keyword_context(keyword, context, sub_context);
                     return -1;
                 }
                 break;
@@ -128,12 +114,7 @@ parse_config_depth(void *context, FILE *cfg, const struct Keyword *grammar,
 
                     if (result <= 0) {
                         /* Free sub_context if it was newly created and not finalized */
-                        if (keyword->create && sub_context != context) {
-                            if (keyword->cleanup)
-                                keyword->cleanup(sub_context);
-                            else
-                                free(sub_context);
-                        }
+                        cleanup_keyword_context(keyword, context, sub_context);
                         return result;
                     }
 
@@ -163,6 +144,7 @@ parse_config_depth(void *context, FILE *cfg, const struct Keyword *grammar,
                 }
                 return 1;
             case TOKEN_END:
+                cleanup_keyword_context(keyword, context, sub_context);
                 return 1;
         }
     }
@@ -182,4 +164,14 @@ find_keyword(const struct Keyword *grammar, const char *word) {
         return grammar;
 
     return NULL;
+}
+
+static void
+cleanup_keyword_context(const struct Keyword *keyword, void *context, void *sub_context) {
+    if (keyword && keyword->create && sub_context && sub_context != context) {
+        if (keyword->cleanup)
+            keyword->cleanup(sub_context);
+        else
+            free(sub_context);
+    }
 }
